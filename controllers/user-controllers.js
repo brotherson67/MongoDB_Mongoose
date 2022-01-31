242c17bfc1751ea1d95f90c09c4648ccb13a7386
brotherson67 <mitchell.brotherson@gmail.com>
2022-01-30 23:21:41 -0700

    created some user routes

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -23,9 +23,31 @@ const userController = {
   },
 
   // Create User
+  createUser({ body }, res) {
+    users
+      .create(body)
+      .then((dbUserData) => {
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
 
   // Update User
-
+  updateUser({ params, body }, res) {
+    users
+      .findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "User not found." });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
   // Delete User
 };
 
