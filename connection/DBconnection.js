ecefddc21210fe312a4b7cfc4bacd4803746e9c1
Mitch Brotherson <89677641+brotherson67@users.noreply.github.com>
2022-02-02 12:33:55 -0700

    Delete connection directory

--- connection/DBconnection.js
+++
@@ -1,9 +0,0 @@
-const Mongoose = require("mongoose");
-
-async function dbConnect() {
-  return Mongoose.connect(
-    process.env.MOGODB_URI || "mongodb://localhost/smbackend"
-  );
-}
-
-module.exports = dbConnect;
